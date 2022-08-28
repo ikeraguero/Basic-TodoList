94b59bac8669da85847ccddd15b3b81dfd337ab7
ikeraguero <ikerpires407@gmail.com>
2022-08-28 14:48:41 -0300

    update

--- app.js
+++ app.js
@@ -16,7 +16,21 @@ function addTodo(event){
     todoDiv.classList.add('todo');
     //Create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo)
+    //Check-mark Button
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton)
+    //Trash Button
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton)
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear todo input
+    todoInput.value = "";
 }
